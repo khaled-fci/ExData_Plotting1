4314b9352db5f592a800f241aac2eeb06ad890de
Khaled <khaled_fci@hotmail.com>
2015-12-13 09:49:32 +0200

    Adding all files

--- plot2.R
+++ plot2.R
@@ -3,5 +3,5 @@ data2 <- data[data$Date %in% c("1/2/2007", "2/2/2007"),]
 png(file="plot2.png",width=480, height=480)
 datetime <- strptime(paste(data2$Date, data2$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 gap <- as.numeric(data2$Global_active_power)
-plot(x=datetime, y=gap, type="l", xlab="", ylab="Global Active Power (kilowatts")
+plot(x=datetime, y=gap, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
---
+++ plot3.R
@@ -0,0 +1,12 @@
+data <- read.csv("household_power_consumption.txt", sep=";", colClasses = "character")
+data2 <- data[data$Date %in% c("1/2/2007", "2/2/2007"),]
+png(file="plot3.png",width=480, height=480)
+datetime <- strptime(paste(data2$Date, data2$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+sub1 <- as.numeric(data2$Sub_metering_1)
+sub2 <- as.numeric(data2$Sub_metering_2)
+sub3 <- as.numeric(data2$Sub_metering_3)
+plot(x=datetime, y=sub1, type="l", xlab="", ylab="Energy sub metering")
+points(x=datetime, y=sub2, type="l", col="red")
+lines(x=datetime, y=sub3, type="l", col="blue")
+legend("topright", lty=c(1,1), col=c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
